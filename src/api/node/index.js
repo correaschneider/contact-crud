5df4a6a5b29bdb0f4ccdeec0531c1751fd07cd51
Pedro Schneider <pedro.schneider@cwi.com.br>
2019-09-12 11:06:01 -0300

    Abrindo CORS para acesso do APP

--- src/api/node/index.js
+++ src/api/node/index.js
@@ -8,6 +8,15 @@ const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    next();
+});
+
 const routes = require('./modules')
 app.use(routes)
 
