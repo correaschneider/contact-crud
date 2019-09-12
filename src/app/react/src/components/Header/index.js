480a8170f3538c153b2a5d71766eea3690a7576b
Pedro Schneider <pedro.schneider@cwi.com.br>
2019-09-12 13:09:34 -0300

    Adicionando listagem e paginaçao dos contatos

--- src/app/react/src/components/Header/index.js
+++ src/app/react/src/components/Header/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './styles.css'
 
-const Header = () => <header id="main-header">Teste</header>
+const Header = () => <header id="main-header">Contacts</header>
 
 export default Header
--- src/app/react/src/pages/main/index.js
+++ src/app/react/src/pages/main/index.js
@@ -1,17 +1,64 @@
 import React, { Component } from 'react'
-
 import api from '../../services/api'
 
+import './style.css'
+
 export default class Main extends Component {
+    state = {
+        contacts: [],
+        pageInfo: {}
+    }
+
     componentDidMount () {
         this.loadContacts()
     }
 
-    loadContacts = async () => {
-        console.log(await api.get('/contact'))
+    loadContacts = async (page = 1) => {
+        let response = await api.get(`/contact?page=${page}`)
+        
+        let { docs, ...pageInfo } = response.data
+
+        this.setState({ contacts: docs, pageInfo })
+    }
+
+    prevPage = () => {
+        let { pageInfo: {page, pages} } = this.state
+
+        if (page === 1) return
+
+        page--
+
+        this.loadContacts(page)
+    }
+
+    nextPage = () => {
+        let { pageInfo: {page, pages} } = this.state
+
+        if (page === pages) return
+
+        page++
+
+        this.loadContacts(page)
     }
 
     render () {
-        return <h1>Testae</h1>
+        const { contacts, pageInfo: {page, pages} } = this.state
+
+        return (
+            <div className="contact-list">
+                {contacts.map(contact => (
+                    <article key={contact._id}>
+                        <strong>{contact.name}</strong>
+
+                        <a href="">Acessar</a>
+                    </article>
+                ))}
+
+                <div className="actions">
+                    <button disabled={page === 1} onClick={this.prevPage}>Anterior</button>
+                    <button disabled={page === pages} onClick={this.nextPage}>Próxima</button>
+                </div>
+            </div>
+        )
     }
 }
